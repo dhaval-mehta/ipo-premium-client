ac7e93f600ce54aeaec8e6c4191a425d1ae5562e
Dhaval Mehta <20968146+dhaval-mehta@users.noreply.github.com>
2025-04-30 13:02:22 +0530

    remove ncd and buyback models

--- models.py
+++ models.py
@@ -19,45 +19,6 @@ class IPO:
 
         return ''
 
-
-class NCD:
-    def __init__(self, **kwargs) -> None:
-        super().__init__()
-        self.id = kwargs.get('id')
-        self.name = kwargs.get('name')
-        self.open_date = kwargs.get('open_date')
-        self.close_date = kwargs.get('close_date')
-        self.base_size = kwargs.get('base_size')
-        self.shelf_size = kwargs.get('shelf_size')
-        self.rating = kwargs.get('rating')
-
-
-class BuyBack:
-    def __init__(self, **kwargs) -> None:
-        super().__init__()
-        self.id = kwargs.get('id')
-        self.name = kwargs.get('name')
-        self.record_date = kwargs.get('record_date')
-        self.open_date = kwargs.get('open_date')
-        self.close_date = kwargs.get('close_date')
-        self.buy_back_price = kwargs.get('buy_back_price')
-        self.market_price = kwargs.get('market_price')
-        self.issue_size = kwargs.get('issue_size')
-
-    @property
-    def gain(self):
-        if self.market_price and self.buy_back_price:
-            return round(self.buy_back_price - self.market_price, 2)
-        else:
-            return ''
-
-    @property
-    def gain_percentage(self):
-        if self.market_price and self.buy_back_price:
-            return round(self.gain * 100 / self.market_price, 2)
-        else:
-            return ''
-
 class Subscription:
     def __init__(self, shared_offered: int, shared_bid_for: int, bid_amount: float) -> None:
         super().__init__()
